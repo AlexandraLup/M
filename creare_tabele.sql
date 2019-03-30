49ef63e0e4d57125714db550c00acb991f7e2f18
dianagabr <focadianagabriela@yahoo.com>
2019-03-30 23:32:02 +0200

    Create creare_tabele.sql

---
+++ creare_tabele.sql
@@ -0,0 +1,125 @@
+
+DROP TABLE angajati CASCADE CONSTRAINTS
+/
+DROP TABLE clienti CASCADE CONSTRAINTS
+/
+DROP TABLE facturi CASCADE CONSTRAINTS
+/
+DROP TABLE produse CASCADE CONSTRAINTS
+/
+DROP TABLE stocuri_magazin CASCADE CONSTRAINTS
+/
+DROP TABLE magazine CASCADE CONSTRAINTS
+/
+DROP TABLE evidente CASCADE CONSTRAINTS
+/
+DROP TABLE contracte CASCADE CONSTRAINTS
+/
+DROP TABLE cheltuieli CASCADE CONSTRAINTS
+/
+DROP TABLE venituri CASCADE CONSTRAINTS
+/
+CREATE TABLE magazine (
+  id INT NOT NULL PRIMARY KEY,
+  adresa VARCHAR2(100) NOT NULL,
+  telefon  VARCHAR2(11) NOT NULL,
+  nr_angajati INT
+)
+/
+
+CREATE TABLE angajati (
+  id INT NOT NULL PRIMARY KEY,
+  id_magazin INT NOT NULL,
+  nume VARCHAR2(15) NOT NULL,
+  prenume VARCHAR2(30) NOT NULL,
+  salariu NUMBER(10,2),
+  functie VARCHAR2(20),
+  data_angajare DATE,
+ CONSTRAINT fk_angajati_id_magazin FOREIGN KEY (id_magazin) REFERENCES magazine(id)
+)
+/
+
+CREATE TABLE clienti (
+  id INT NOT NULL PRIMARY KEY,
+  nume VARCHAR2(15) NOT NULL,
+  prenume VARCHAR2(30) NOT NULL,
+  data_inregistrare DATE
+)
+/
+create table produse(
+  id INT PRIMARY KEY,
+  categorie  VARCHAR2(30),
+  denumire  VARCHAR2(100),
+  model_produs VARCHAR2(30),
+  culoare VARCHAR2(30),
+  pret NUMBER(10,2)
+);
+/
+create table stocuri_magazin(
+id INT PRIMARY KEY,
+id_magazin INT NOT NULL,
+id_produs INT NOT NULL,
+stoc_XS INT,
+stoc_S INT,
+stoc_M INT,
+stoc_L INT,
+stoc_XL INT,
+CONSTRAINT fk_stocuri_magazin_id_magazin FOREIGN KEY (id_magazin) REFERENCES magazine(id),
+CONSTRAINT fk_stocuri_magazin_id_produs FOREIGN KEY (id_produs) REFERENCES produse(id)
+);
+/
+
+
+
+drop type lista_produse force;
+/
+create or replace type produs as object(
+  id_produs number(10),
+  denumire_produs varchar2(30),
+  pret_bucata number(10,2),
+  cantitate number(5)
+  );
+/
+create or replace TYPE lista_produse IS TABLE OF produs;
+/
+
+create table facturi(
+  id INT PRIMARY KEY,
+  id_angajat INT NOT NULL,
+  id_client INT NOT NULL,
+  data_factura DATE,
+  valoare NUMBER(10,2),
+  lista_produse lista_produse,
+  CONSTRAINT fk_facturi_id_angajat FOREIGN KEY (id_angajat) REFERENCES angajati(id),
+  CONSTRAINT fk_facturi_id_client FOREIGN KEY (id_client) REFERENCES clienti(id)
+) nested table lista_produse store as lista;
+/
+CREATE TABLE evidente (
+  id INT NOT NULL PRIMARY KEY,
+  id_angajat INT,
+  data  DATE,
+  CONSTRAINT fk_evidente_id_angajat FOREIGN KEY (id_angajat) REFERENCES angajati(id)
+)
+/
+CREATE TABLE contracte (
+  id INT NOT NULL PRIMARY KEY,
+  id_angajat INT,
+  data_inceput  DATE,
+  data_sfarsit  DATE,
+  CONSTRAINT fk_contracte_id_angajat FOREIGN KEY (id_angajat) REFERENCES angajati(id)
+)
+/
+CREATE TABLE cheltuieli (
+  id INT NOT NULL PRIMARY KEY,
+  id_evidenta INT,
+  valoare NUMBER(10,2),
+  CONSTRAINT fk_cheluieli_id_evidenta FOREIGN KEY (id_evidenta) REFERENCES evidente(id)
+)
+/
+CREATE TABLE venituri (
+  id INT NOT NULL PRIMARY KEY,
+  id_evidenta INT NOT NULL,
+  valoare NUMBER(10,2),
+  CONSTRAINT fk_venituri_id_evidenta FOREIGN KEY (id_evidenta) REFERENCES evidente(id)
+)
+/
