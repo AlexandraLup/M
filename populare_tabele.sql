ad0f631ec49fa5309e4709deec80974abfa350c2
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-03-31 11:13:48 +0200

    Update populare_tabele.sql

--- populare_tabele.sql
+++ populare_tabele.sql
@@ -76,6 +76,38 @@ v_m INT;
 v_l INT;
 v_xl INT;
 v_id_produs INT;
+
+
+
+lista_nume varr := varr('Ababei','Acasandrei','Adascalitei','Afanasie','Agafitei','Agape','Aioanei','Alexandrescu','Alexandru','Alexe','Alexii','Amarghioalei','Ambroci','Andonesei','Andrei','Andrian','Andrici','Andronic','Andros','Anghelina','Anita','Antochi','Antonie','Apetrei','Apostol','Arhip','Arhire','Arteni','Arvinte','Asaftei','Asofiei','Aungurenci','Avadanei','Avram','Babei','Baciu','Baetu','Balan','Balica','Banu','Barbieru','Barzu','Bazgan','Bejan','Bejenaru','Belcescu','Belciuganu','Benchea','Bilan','Birsanu','Bivol','Bizu','Boca','Bodnar','Boistean','Borcan','Bordeianu','Botezatu','Bradea','Braescu','Budaca','Bulai','Bulbuc-aioanei','Burlacu','Burloiu','Bursuc','Butacu','Bute','Buza','Calancea','Calinescu','Capusneanu','Caraiman','Carbune','Carp','Catana','Catiru','Catonoiu','Cazacu','Cazamir','Cebere','Cehan','Cernescu','Chelaru','Chelmu','Chelmus','Chibici','Chicos','Chilaboc','Chile','Chiriac','Chirila','Chistol','Chitic','Chmilevski','Cimpoesu','Ciobanu','Ciobotaru','Ciocoiu','Ciofu','Ciornei','Citea','Ciucanu','Clatinici','Clim','Cobuz','Coca','Cojocariu','Cojocaru','Condurache','Corciu','Corduneanu','Corfu','Corneanu','Corodescu','Coseru','Cosnita','Costan','Covatariu','Cozma','Cozmiuc','Craciunas','Crainiceanu','Creanga','Cretu','Cristea','Crucerescu','Cumpata','Curca','Cusmuliuc','Damian','Damoc','Daneliuc','Daniel','Danila','Darie','Dascalescu','Dascalu','Diaconu','Dima','Dimache','Dinu','Dobos','Dochitei','Dochitoiu','Dodan','Dogaru','Domnaru','Dorneanu','Dragan','Dragoman','Dragomir','Dragomirescu','Duceac','Dudau','Durnea','Edu','Eduard','Eusebiu','Fedeles','Ferestraoaru','Filibiu','Filimon','Filip','Florescu','Folvaiter','Frumosu','Frunza','Galatanu','Gavrilita','Gavriliuc','Gavrilovici','Gherase','Gherca','Ghergu','Gherman','Ghibirdic','Giosanu','Gitlan','Giurgila','Glodeanu','Goldan','Gorgan','Grama','Grigore','Grigoriu','Grosu','Grozavu','Gurau','Haba','Harabula','Hardon','Harpa','Herdes','Herscovici','Hociung','Hodoreanu','Hostiuc','Huma','Hutanu','Huzum','Iacob','Iacobuta','Iancu','Ichim','Iftimesei','Ilie','Insuratelu','Ionesei','Ionesi','Ionita','Iordache','Iordache-tiroiu','Iordan','Iosub','Iovu','Irimia','Ivascu','Jecu','Jitariuc','Jitca','Joldescu','Juravle','Larion','Lates','Latu','Lazar','Leleu','Leon','Leonte','Leuciuc','Leustean','Luca','Lucaci','Lucasi','Luncasu','Lungeanu','Lungu','Lupascu','Lupu','Macariu','Macoveschi','Maftei','Maganu','Mangalagiu','Manolache','Manole','Marcu','Marinov','Martinas','Marton','Mataca','Matcovici','Matei','Maties','Matrana','Maxim','Mazareanu','Mazilu','Mazur','Melniciuc-puica','Micu','Mihaela','Mihai','Mihaila','Mihailescu','Mihalachi','Mihalcea','Mihociu','Milut','Minea','Minghel','Minuti','Miron','Mitan','Moisa','Moniry-abyaneh','Morarescu','Morosanu','Moscu','Motrescu','Motroi','Munteanu','Murarasu','Musca','Mutescu','Nastaca','Nechita','Neghina','Negrus','Negruser','Negrutu','Nemtoc','Netedu','Nica','Nicu','Oana','Olanuta','Olarasu','Olariu','Olaru','Onu','Opariuc','Oprea','Ostafe','Otrocol','Palihovici','Pantiru','Pantiruc','Paparuz','Pascaru','Patachi','Patras','Patriche','Perciun','Perju','Petcu','Pila','Pintilie','Piriu','Platon','Plugariu','Podaru','Poenariu','Pojar','Popa','Popescu','Popovici','Poputoaia','Postolache','Predoaia','Prisecaru','Procop','Prodan','Puiu','Purice','Rachieru','Razvan','Reut','Riscanu','Riza','Robu','Roman','Romanescu','Romaniuc','Rosca','Rusu','Samson','Sandu','Sandulache','Sava','Savescu','Schifirnet','Scortanu','Scurtu','Sfarghiu','Silitra','Simiganoschi','Simion','Simionescu','Simionesei','Simon','Sitaru','Sleghel','Sofian','Soficu','Sparhat','Spiridon','Stan','Stavarache','Stefan','Stefanita','Stingaciu','Stiufliuc','Stoian','Stoica','Stoleru','Stolniceanu','Stolnicu','Strainu','Strimtu','Suhani','Tabusca','Talif','Tanasa','Teclici','Teodorescu','Tesu','Tifrea','Timofte','Tincu','Tirpescu','Toader','Tofan','Toma','Toncu','Trifan','Tudosa','Tudose','Tuduri','Tuiu','Turcu','Ulinici','Unghianu','Ungureanu','Ursache','Ursachi','Urse','Ursu','Varlan','Varteniuc','Varvaroi','Vasilache','Vasiliu','Ventaniuc','Vicol','Vidru','Vinatoru','Vlad','Voaides','Vrabie','Vulpescu','Zamosteanu','Zazuleac');
+  lista_prenume varr := varr( 'Adina','Alexandra','Alina','Ana','Anca','Anda','Andra','Andreea','Andreia','Antonia','Bianca','Camelia','Claudia','Codrina','Cristina','Daniela','Daria','Delia','Denisa','Diana','Ecaterina','Elena','Eleonora','Elisa','Ema','Emanuela','Emma','Gabriela','Georgiana','Ileana','Ilona','Ioana','Iolanda','Irina','Iulia','Iuliana','Larisa','Laura','Loredana','Madalina','Malina','Manuela','Maria','Mihaela','Mirela','Monica','Oana','Paula','Petruta','Raluca','Sabina','Sanziana','Simina','Simona','Stefana','Stefania','Tamara','Teodora','Theodora','Vasilica','Xena', 'Adrian','Alex','Alexandru','Alin','Andreas','Andrei','Aurelian','Beniamin','Bogdan','Camil','Catalin','Cezar','Ciprian','Claudiu','Codrin','Constantin','Corneliu','Cosmin','Costel','Cristian','Damian','Dan','Daniel','Danut','Darius','Denise','Dimitrie','Dorian','Dorin','Dragos','Dumitru','Eduard','Elvis','Emil','Ervin','Eugen','Eusebiu','Fabian','Filip','Florian','Florin','Gabriel','George','Gheorghe','Giani','Giulio','Iaroslav','Ilie','Ioan','Ion','Ionel','Ionut','Iosif','Irinel','Iulian','Iustin','Laurentiu','Liviu','Lucian','Marian','Marius','Matei','Mihai','Mihail','Nicolae','Nicu','Nicusor','Octavian','Ovidiu','Paul','Petru','Petrut','Radu','Rares','Razvan','Richard','Robert','Roland','Rolland','Romanescu','Sabin','Samuel','Sebastian','Sergiu','Silviu','Stefan','Teodor','Teofil','Theodor','Tudor','Vadim','Valentin','Valeriu','Vasile','Victor','Vlad','Vladimir','Vladut');
+  lista_functii varr := varr('Manager','Contabil', 'Vanzator', 'Agent-paza', 'Casier', 'Sef-raion', 'Sef-magazin','Asistent-sef-magazin','Sef-depozit','Asistent-sef-depozit','Sef-tura', 'Asistent-vanzari','Gestionar-magazin' );
+  
+  v_iterator number;
+  
+  v_nume VARCHAR2(30);
+  v_prenume varchar2(30);
+  v_functie varchar2(40);
+  
+  v_my_number_prenume number;
+  v_my_number_nume number;
+  v_my_number_functie number;
+  
+  v_verific_id_pers number;
+  v_id_pers number := 1;
+  v_id_clienti number := 1;
+  
+  v_data_angajare date;  
+  v_data_inregistrare date;
+  v_data_factura date;
+  
+  v_salariu number;
+  
+  v_nr_angajati1 INT;
+  v_nr_angajati2 INT;
+  v_id_magazin1 INT;
+ 
+
 BEGIN
  DBMS_OUTPUT.PUT_LINE('Inserare magazine...');
 FOR v_i in 1..100 LOOP
@@ -119,4 +151,73 @@ FOR v_i in 1..100 LOOP
  END LOOP;
 END LOOP;
 DBMS_OUTPUT.PUT_LINE('Inserare stocuri_magazin..Gata!');
+
+
+  --ANGAJATI
+  for v_iterator_magazine in 1..100 loop
+      select nr_angajati, id into v_nr_angajati2, v_id_magazin1 from magazine where id=v_iterator_magazine;
+      for v_iterator in 1..v_nr_angajati2  loop   
+        --nume
+         v_my_number_nume := TRUNC(DBMS_RANDOM.VALUE( 1, lista_nume.count));
+         v_nume := lista_nume(v_my_number_nume);
+         
+         --prenume
+         v_my_number_prenume := TRUNC(DBMS_RANDOM.VALUE( 1, lista_prenume.count));
+         v_prenume := lista_prenume(v_my_number_prenume);
+         
+         --id
+         select count(*) into v_verific_id_pers from (select * from angajati WHERE id = v_id_pers);
+         while (v_verific_id_pers > 0) loop
+              v_id_pers := v_id_pers + 1; 
+              select count(*) into v_verific_id_pers from (select * from angajati WHERE id = v_id_pers);
+         end loop;
+        
+         
+         --functie
+         v_my_number_functie := TRUNC(DBMS_RANDOM.VALUE( 1, lista_functii.count));
+         v_functie := lista_functii(v_my_number_functie);
+         
+         --salariu
+         if(v_functie = 'Manager' or v_functie = 'Contabil') then
+            v_salariu := FLOOR(DBMS_RANDOM.VALUE(50,99)) || FLOOR(DBMS_RANDOM.VALUE(0,9)) || FLOOR(DBMS_RANDOM.VALUE(0,9)) ;
+         else
+            v_salariu := FLOOR(DBMS_RANDOM.VALUE(10,49)) || FLOOR(DBMS_RANDOM.VALUE(0,9)) || FLOOR(DBMS_RANDOM.VALUE(0,9)) ;
+         end if;
+         
+         --data_angajare
+         v_data_angajare := (sysdate-TRUNC(DBMS_RANDOM.VALUE(0,1000)));
+         
+         insert into angajati (id, id_magazin, nume, prenume,salariu, functie,data_angajare) values ( v_id_pers, v_id_magazin1, v_nume, v_prenume,v_salariu, v_functie, v_data_angajare);
+         
+      end loop;
+  end loop;
+  
+   --CLIENTI
+   for v_iterator in 1..300000 loop
+      --nume
+       v_my_number_nume := TRUNC(DBMS_RANDOM.VALUE( 1, lista_nume.count));
+       v_nume := lista_nume(v_my_number_nume);
+       
+       --prenume
+       v_my_number_prenume := TRUNC(DBMS_RANDOM.VALUE( 1, lista_prenume.count));
+       v_prenume := lista_prenume(v_my_number_prenume);
+       
+       --id
+       select count(*) into v_verific_id_pers from (select * from clienti WHERE id = v_id_clienti);
+       while (v_verific_id_pers > 0) loop
+            v_id_clienti := v_id_clienti + 1; 
+            select count(*) into v_verific_id_pers from (select * from clienti WHERE id = v_id_clienti);
+       end loop;
+       
+       
+       --data_inregistrare
+       v_data_inregistrare := (sysdate-TRUNC(DBMS_RANDOM.VALUE(0,500)));
+       
+       
+       insert into clienti (id, nume, prenume, data_inregistrare) values ( v_id_clienti, v_nume, v_prenume, v_data_inregistrare);    
+   end loop;
+  
 END;
+
+
+
