42b2778fbdd8dd50da1ba0e882949d130acbeff2
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-04-02 22:51:24 +0200

    Update creare_tabele.sql

--- creare_tabele.sql
+++ creare_tabele.sql
@@ -13,6 +13,8 @@ DROP TABLE stocuri_magazin CASCADE CONSTRAINTS
 /
 DROP TABLE magazine CASCADE CONSTRAINTS
 /
+DROP TABLE angajati_magazin CASCADE CONSTRAINTS
+/
 DROP TABLE evidente CASCADE CONSTRAINTS
 /
 DROP TABLE contracte CASCADE CONSTRAINTS
@@ -20,6 +22,10 @@ DROP TABLE contracte CASCADE CONSTRAINTS
 DROP TABLE cheltuieli CASCADE CONSTRAINTS
 /
 DROP TABLE venituri CASCADE CONSTRAINTS
+/
+DROP TABLE contabili CASCADE CONSTRAINTS
+/
+DROP TABLE manageri CASCADE CONSTRAINTS
 /
 CREATE TABLE magazine (
   id INT NOT NULL PRIMARY KEY,
@@ -31,13 +37,19 @@ CREATE TABLE magazine (
 
 CREATE TABLE angajati (
   id INT NOT NULL PRIMARY KEY,
-  id_magazin INT NOT NULL,
   nume VARCHAR2(15) NOT NULL,
   prenume VARCHAR2(30) NOT NULL,
   salariu NUMBER(10,2),
   functie VARCHAR2(20),
-  data_angajare DATE,
- CONSTRAINT fk_angajati_id_magazin FOREIGN KEY (id_magazin) REFERENCES magazine(id)
+  data_angajare DATE
+)
+/
+CREATE TABLE angajati_magazin(
+  id INT PRIMARY KEY,
+  id_magazin INT NOT NULL,
+  id_angajat INT NOT NULL,
+  CONSTRAINT fk_angajati_magazin_id_magazin FOREIGN KEY (id_magazin) REFERENCES magazine(id),
+  CONSTRAINT fk_angajati_magazin_id_angajat FOREIGN KEY (id_angajat) REFERENCES angajati(id)
 )
 /
 
@@ -50,7 +62,6 @@ CREATE TABLE clienti (
 /
 create table produse(
   id INT PRIMARY KEY,
-  categorie  VARCHAR2(30),
   denumire  VARCHAR2(100),
   model_produs VARCHAR2(30),
   culoare VARCHAR2(30),
@@ -73,12 +84,8 @@ CONSTRAINT fk_stocuri_magazin_id_produs FOREIGN KEY (id_produs) REFERENCES produ
 
 create table facturi(
   id INT PRIMARY KEY,
-  id_angajat INT NOT NULL,
-  id_client INT NOT NULL,
   data_factura DATE,
-  valoare NUMBER(10,2),
-  CONSTRAINT fk_facturi_id_angajat FOREIGN KEY (id_angajat) REFERENCES angajati(id),
-  CONSTRAINT fk_facturi_id_client FOREIGN KEY (id_client) REFERENCES clienti(id)
+  valoare NUMBER(10,2)
 )
 /
 
@@ -93,17 +100,13 @@ create table lista_produse(
 
 CREATE TABLE evidente (
   id INT NOT NULL PRIMARY KEY,
-  id_angajat INT,
-  data  DATE,
-  CONSTRAINT fk_evidente_id_angajat FOREIGN KEY (id_angajat) REFERENCES angajati(id)
+  data  DATE
 )
 /
 CREATE TABLE contracte (
   id INT NOT NULL PRIMARY KEY,
-  id_angajat INT,
   data_inceput  DATE,
-  data_sfarsit  DATE,
-  CONSTRAINT fk_contracte_id_angajat FOREIGN KEY (id_angajat) REFERENCES angajati(id)
+  data_sfarsit  DATE
 )
 /
 CREATE TABLE cheltuieli (
@@ -120,5 +123,41 @@ CREATE TABLE venituri (
   valoare NUMBER(10,2),
   descriere VARCHAR2(30),
   CONSTRAINT fk_venituri_id_evidenta FOREIGN KEY (id_evidenta) REFERENCES evidente(id)
+)
+/
+CREATE TABLE contabili(
+  id INT NOT NULL PRIMARY KEY,
+  id_angajat INT,
+  id_evidenta INT,
+  CONSTRAINT fk_contabili_id_angajat FOREIGN KEY (id_angajat) REFERENCES angajati(id),
+  CONSTRAINT fk_contabili_id_evidenta FOREIGN KEY (id_evidenta) REFERENCES evidente(id)
+)
+/
+CREATE TABLE manageri(
+  id INT NOT NULL PRIMARY KEY,
+  id_angajat INT,
+  id_contract INT,
+  CONSTRAINT fk_managerii_id_angajat FOREIGN KEY (id_angajat) REFERENCES angajati(id),
+  CONSTRAINT fk_manageri_id_contract FOREIGN KEY (id_contract) REFERENCES contracte(id)
+)
+/
+DROP TABLE facturi_angajati CASCADE CONSTRAINTS
+/
+CREATE TABLE facturi_angajati(
+  id INT PRIMARY KEY,
+  id_factura INT NOT null,
+  id_angajat INT NOT null,
+  CONSTRAINT fk_facturi_angajati_id_angajat FOREIGN KEY (id_angajat) REFERENCES angajati(id),
+  CONSTRAINT fk_facturi_angajati_id_factura FOREIGN KEY (id_factura) REFERENCES facturi(id)
+)
+/
+DROP TABLE facturi_clienti CASCADE CONSTRAINTS
+/
+CREATE TABLE facturi_clienti(
+  id INT PRIMARY KEY,
+  id_factura INT NOT null ,
+  id_client INT NOT null,
+  CONSTRAINT fk_facturi_clienti_id_client FOREIGN KEY (id_client) REFERENCES clienti(id),
+  CONSTRAINT fk_facturi_clienti_id_factura FOREIGN KEY (id_factura) REFERENCES facturi(id)
 )
 /
