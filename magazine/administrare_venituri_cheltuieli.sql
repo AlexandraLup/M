7bf5ea8a90b36a5cbfd1c91f5c2824cd2137016a
dianagabr <focadianagabriela@yahoo.com>
2019-05-15 12:24:13 +0300

    Update administrare_venituri_cheltuieli.sql

--- magazine/administrare_venituri_cheltuieli.sql
+++ magazine/administrare_venituri_cheltuieli.sql
@@ -2,9 +2,10 @@
 create or replace package administrare_vc as
   function add_venit(p_id_angajat in evidente.id_angajat%type,p_valoare in venituri.valoare%type, p_descriere in venituri.descriere%type ) return varchar2;
   function delete_venit(p_id_venit in venituri.id%TYPE) return varchar2;
-  
+  function cauta_venit(p_id_venit in venituri.id%TYPE) return varchar2;
   function add_cheltuieli(p_id_angajat in evidente.id_angajat%type,p_valoare in cheltuieli.valoare%type, p_descriere in cheltuieli.descriere%type ) return varchar2;
   function delete_cheltuieli(p_id_cheltuieli in cheltuieli.id%TYPE) return varchar2;
+  function cauta_cheltuieli(p_id_cheltuieli in cheltuieli.id%TYPE) return varchar2;
 
 
  end;
@@ -141,5 +142,48 @@ create or replace package body administrare_vc is
       return 'Id-ul cheltuielii nu exista in baza de date!';
       
   end;
+  
+ function cauta_venit(p_id_venit in venituri.id%TYPE) return varchar2 as
+  res varchar2(300) := ' ';
+  v_exception_c number := 0;
+  begin
+  
+    select count(*) into v_exception_c from (select id from venituri where id=p_id_venit);
+  
+    if(v_exception_c = 0) then
+      raise id_venit_inexistent;
+    else
+      for v_info in (select v.id, v.id_evidenta, v.valoare, e.data, a.nume, a.prenume from venituri v join evidente e on e.id=v.id_evidenta join angajati a on a.id=e.id_angajat where v.id=p_id_venit)loop
+      res := res || v_info.id ||' ' || v_info.id_evidenta||' '||v_info.valoare||' ' || v_info.data||' '||v_info.nume||' ' ||v_info.prenume||' ';
+      end loop;
+      return res;
+    end if;
+    exception
+      when id_venit_inexistent then
+      return 'Id-ul venitului nu exista in baza de date!';
+      
+  end; 
+
+   function cauta_cheltuieli(p_id_cheltuieli in cheltuieli.id%TYPE) return varchar2 as
+  res varchar2(300) := ' ';
+  v_exception_c number := 0;
+  begin
+  
+    select count(*) into v_exception_c from (select id from cheltuieli where id=p_id_cheltuieli);
+  
+    if(v_exception_c = 0) then
+      raise id_cheltuieli_inexistent;
+    else
+      for v_info in (select v.id, v.id_evidenta, v.valoare, v.descriere, e.data, a.nume, a.prenume from cheltuieli v join evidente e on e.id=v.id_evidenta join angajati a on a.id=e.id_angajat where v.id=p_id_cheltuieli)loop
+      res := res || v_info.id ||' ' || v_info.id_evidenta||' '||v_info.valoare||' '||v_info.descriere||' '||v_info.data||' '||v_info.nume||' ' ||v_info.prenume||' ';
+      end loop;
+      return res;
+    end if;
+    exception
+      when id_cheltuieli_inexistent then
+      return 'Id-ul cheltuielii nu exista in baza de date!';
+      
+  end; 
+
   
 end administrare_vc ;
