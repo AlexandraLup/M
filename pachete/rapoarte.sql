3ce72ebd9eb50d1596e8737d8586803422879ed0
AlexandraLup <48436138+AlexandraLup@users.noreply.github.com>
2019-05-16 20:38:46 +0200

    Update rapoarte.sql

--- pachete/rapoarte.sql
+++ pachete/rapoarte.sql
@@ -3,9 +3,9 @@
 create or replace package rapoarte as
 	function raport_cheltuieli( p_descriere in cheltuieli.descriere%type, p_data_inceput in evidente.data%type, p_data_sfarsit in evidente.data%type) return varchar2;
    	function raport_venituri( p_data_inceput in evidente.data%type, p_data_sfarsit in evidente.data%type) return varchar2;
-
+    function raport_contracte_expirate(p_id_magazin magazine.id%type, p_data_cautare contracte.data_sfarsit%type) return varchar2;
 end;
-
+/
 create or replace package body rapoarte as
 
 	descriere_inexistenta exception;
@@ -99,4 +99,44 @@ create or replace package body rapoarte as
         return 'Unknown exception';
   end;
   
-  end rapoarte;
+  
+  
+  function raport_contracte_expirate(p_id_magazin magazine.id%type, p_data_cautare contracte.data_sfarsit%type) return varchar2 as
+    
+	cursor report_contracte is  select a.id, a.nume, a.prenume ,c.data_inceput, c.data_sfarsit from contracte c join angajati a on a.id=c.id_angajat where c.data_sfarsit<=p_data_cautare;
+  
+	
+	v_exception_d number := 0;
+	
+	v_fisier UTL_FILE.FILE_TYPE;
+	
+  begin
+	v_fisier:=UTL_FILE.FOPEN('EXPORT','raportContracteExpirate.csv','W');
+  
+    select count(*) into v_exception_d from contracte where data_sfarsit<=p_data_cautare;
+	
+    if (v_exception_d = 0) then 
+		raise data_inexistenta;
+	else
+	   
+	    FOR v_std_linie IN report_contracte LOOP     
+			UTL_FILE.PUTF(v_fisier, v_std_linie.id || ',' || v_std_linie.nume || ',' || v_std_linie.data_inceput|| ',' || v_std_linie.data_sfarsit || '\n');
+		END LOOP;
+	
+		UTL_FILE.FCLOSE(v_fisier);
+		return 'Success';
+	
+    end if;
+	
+	
+    exception
+	when data_inexistenta then
+        return 'Nu exista contracte expirate pana la data aleasa';
+      when others then
+        return 'Unknown exception';
+    end;
+  
+end rapoarte;
+  
+  
+  
